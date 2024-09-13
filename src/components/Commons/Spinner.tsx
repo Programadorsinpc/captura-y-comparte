dadf2833320fad39904e462c9f5ad346750c607c
Nicolas Lopez <nicolas@iztech.co>
2024-09-13 10:56:09 -0500

    Spinner component

---
+++ src/components/Commons/Spinner.tsx
@@ -0,0 +1,8 @@
+const Spinner = () => (
+    <div className="flex items-center justify-center h-screen">
+      <div className="border-t-4 border-blue-500 border-solid w-16 h-16 border-radius-full animate-spin"></div>
+    </div>
+  );
+  
+  export default Spinner;
+  
